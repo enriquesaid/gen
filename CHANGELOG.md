9dcf4625bf83390e11037364cf48f757039c55bc
Enrique Marques Jr <enr@esaid.com.br>
2020-08-11 12:20:20 -0300

    0.0.1+2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.1+2
+
+- Fix example
+
 ## 0.0.1+1
 
 - Added homepage and repository in `pubspec.yaml`
--- lib/gen.dart
+++ lib/gen.dart
@@ -1,6 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library gen;
 
 export 'src/gen_base.dart';
