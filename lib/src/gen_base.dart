e6175191589c9b75ad2c1f7fbed1661cf191a55d
Enrique Marques Jr <enr@esaid.com.br>
2020-08-11 18:57:08 -0300

    0.0.1+6

--- lib/src/gen_base.dart
+++ lib/src/gen_base.dart
@@ -1,9 +1,6 @@
 import 'dart:math';
 import 'package:strings/strings.dart';
-
-// TODO: Wrong place
-var text =
-    "Teoria da Relatividade é a denominação dada ao conjunto de duas teorias científicas: a Relatividade Restrita e a Relatividade Geral. A Relatividade Especial é uma teoria publicada no ano de 1905 por Albert Einstein, concluindo estudos precedentes do físico neerlandês Hendrik Lorentz, entre outros. Ela substitui os conceitos independentes de espaço e tempo da Teoria de Newton pela ideia de espaço-tempo como uma entidade geométrica unificada. O espaço-tempo na relatividade especial consiste de uma variedade diferenciável de 4 dimensões, três espaciais e uma temporal, munida de uma métrica pseudo-riemanniana, o que permite que noções de geometria possam ser utilizadas. É nessa teoria, também, que surge a ideia de velocidade da luz invariante. O termo especial é usado porque ela é um caso particular do princípio da relatividade em que efeitos da gravidade são ignorados. Dez anos após a publicação da teoria especial, Einstein publicou a Teoria Geral da Relatividade, que é a versão mais ampla da teoria, em que os efeitos da gravitação são integrados, surgindo a noção de espaço-tempo curvo. Cor é como o cérebro interpreta os sinais eletro nervosos vindos do olho, resultantes da reemissão da luz vinda de um objeto que foi emitida por uma fonte luminosa por meio de ondas eletromagnéticas; e que corresponde à parte do espectro eletromagnético que é visível. A Cor não é um fenômeno físico. Um mesmo comprimento de onda pode ser percebido diferentemente por diferentes pessoas, ou seja, cor é um fenômeno fisiológico, de caráter subjetivo e individual. Rey Skywalker, nascida Rey Palpatine, era uma Humana catadora de sucata nativa de Jakku que descobriu ser sensível à Força durante sua busca ao lendário Mestre Jedi Luke Skywalker a fim de trazer uma nova esperança à galáxia à beira da guerra. Rey cresceu sozinha no mundo desértico de Jakku, esperando sua família retornar, até encontrar BB-8, droide que carregava informações sobre o paradeiro de Skywalker. Juntamente com Finn, um desertor da Primeira Ordem, Han Solo e Chewbacca, ela embarcou em uma jornada para entregar o mapa a Skywalker para General Leia Organa e para a Resistência. No caminho, ela descobriu o sabre de luz de Skywalker e sua sensibilidade à Força despertou. Logo depois, ela acabou sendo capturada por Kylo Ren, nascido Ben Solo, filho de Leia e Han, contra quem Rey duelou na Base Starkiller logo antes da Resistência destruir a superarma. Depois de chegar à base da Resistência em D'Qar, Leia deu a Rey a missão de encontrar Luke com base no mapa e apresentar a ele o seu antigo sabre de luz como um símbolo da única esperança da galáxia: a volta dos Jedi.";
+import 'data/texts/default.dart';
 
 class Generate {
   final _r = Random();
@@ -17,7 +14,7 @@ class Generate {
     return i < min ? min : i;
   }
 
-  List<String> words() => text.split(' ')..shuffle(_r);
+  List<String> words() => dataTextsDefault.split(' ')..shuffle(_r);
 
   String firstWord() => capitalize(words()[0]).replaceAll('.', '');
 
@@ -27,8 +24,8 @@ class Generate {
   String title() {
     var data = words();
     var first = firstWord();
-    var start = integer(data.length);
     var range = integer(7, 1);
+    var start = integer(data.length);
     var end = start + range > data.length ? data.length : start + range;
 
     return [
@@ -43,7 +40,8 @@ class Generate {
   }
 
   String paragraph() {
-    var p = capitalize((text.split('. ')..shuffle(_r))[0]);
+    var p = capitalize((dataTextsDefault.split('. ')..shuffle(_r))[0])
+        .replaceAll('.', '');
     return '$p.';
   }
 }
